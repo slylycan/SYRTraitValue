557a9fbf02511ec73b12bcd416114f96781fbc7f
Syrchalis <syrchalis@gmx.de>
2021-04-06 02:23:45 +0200

    Initial commit

---
+++ Source/TraitCounter.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace SyrTraitValue
+{
+    [StaticConstructorOnStartup]
+    public static class TraitCounter
+    {
+        static TraitCounter()
+        {
+            CountTraits();
+        }
+
+        public static void CountTraits()
+        {
+            int traitsInTotal = 0;
+            int traitsWithValue = 0;
+            foreach (TraitDef t in DefDatabase<TraitDef>.AllDefs)
+            {
+                int degrees = t.degreeDatas.Count;
+                traitsInTotal += degrees;
+                TraitValueExtension modExtension = t.GetModExtension<TraitValueExtension>();
+                if (modExtension != null)
+                {
+                    int traitValues = modExtension.traitValues.Count();
+                    traitsWithValue += traitValues;
+                    if (degrees != modExtension.traitValues.Count())
+                    {
+                        Log.Warning("WARNING: Trait " + t.LabelCap + " has " + degrees + " degrees, but " + traitValues + ". You should configure one value per trait degree data.");
+                    }
+                }
+            }
+
+            Log.Message("Trait Value Trait Counter - Traits: " + traitsInTotal + " | Traits with an assigned value: " + traitsWithValue);
+        }
+    }
+}
---
+++ Source/TraitValueExtension.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace SyrTraitValue
+{
+    public class TraitValueExtension : DefModExtension
+    {
+        public List<int> traitValues;
+    }
+}
